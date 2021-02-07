c3db51f5fbba0b753b9945c3ecd2ec90a1c84a29
Jakob Grøhn <bokajgd@gmail.com>
2021-02-07 19:10:02 +0100

    Adding html map

--- W1-Interactive-Map/README.md
+++ W1-Interactive-Map/README.md
@@ -18,8 +18,8 @@ Depending on the specific research question, multiple data layers could be benef
 3. Fixation points - *Points*<br>
 4. Visual attention distribution heatmap - *Raster layer*<br>
 5. Microsaccade paths - *Polylines* (Provides higher resolution of eye movements during fixation but is rarely used in behavioural research)
-<br><br>
-All these data layers are obtained (thoug data pre-processing may be required) when running experiments using modern eye tracking technology.Specified polygons may be implemented to outline certain areas of interest (AOIs). Adding suchg demarcations could allow for conducting statistical analyses of e.g. amount of time children soend looking at various areas of the screen. Adding saccade paths would enable the researcher to track the temporal order of the changes in visual attention. Adding fixation points would be able to represent points where the child's visual attention remains fixated for a certain amount of times. An extra attribute could be added to these points by seting the size of the symbol according to the length of the fixation. Instead of adding the above polygon/polyline/point layers, the child's visual attention could also be analyzed by mapping the distribution of visual attention across the screen using a raster layer. This would result in a visually pleasing heatmap. The image below shows different data layers added to an analyzed screen.
+<br>
+<br>All these data layers are obtained (thoug data pre-processing may be required) when running experiments using modern eye tracking technology.Specified polygons may be implemented to outline certain areas of interest (AOIs). Adding suchg demarcations could allow for conducting statistical analyses of e.g. amount of time children soend looking at various areas of the screen. Adding saccade paths would enable the researcher to track the temporal order of the changes in visual attention. Adding fixation points would be able to represent points where the child's visual attention remains fixated for a certain amount of times. An extra attribute could be added to these points by seting the size of the symbol according to the length of the fixation. Instead of adding the above polygon/polyline/point layers, the child's visual attention could also be analyzed by mapping the distribution of visual attention across the screen using a raster layer. This would result in a visually pleasing heatmap. The image below shows different data layers added to an analyzed screen.
 <br><br>
 
 ### Example of different data layers applied to the same baselayer
@@ -28,6 +28,9 @@ All these data layers are obtained (thoug data pre-processing may be required) w
 <br>
 Source: https://medium.com/@TheRealTang/subtleties-of-eyetracking-heat-maps-and-gaze-plots-a7ba4207f20f
 <br>
+
+# Task 3 - Creating an interactive map 
+The script containg code for producing the map can be found in the file *W1-Interactive-Map.R*. The final html map can be found be clicking the file *Interactive-Map.html*.
 
 # License
 
--- W1-Interactive-Map/W1-Interactive-Map.R
+++ W1-Interactive-Map/W1-Interactive-Map.R
@@ -1,1 +1,101 @@
 # Creating an interactive browser-accessible html map of The Blue Mountains
+
+# Importing packages
+# Uncomment and run below line if pacaman is not already installed
+# instal.packages('pacman')
+library(pacman)
+p_load(leaflet, htmltools, htmlwidgets, tidyverse)
+
+# Reading in the provided data
+data <- read_csv("data/RCFeature.csv")
+
+
+# A single location is missing coordinates and must be removed
+data<- data %>% filter(!is.na(Longitude)) %>% filter(!is.na(Latitude))
+
+
+#### CREATING MAP ####
+
+# Setting the baseline location
+center_location <-  leaflet() %>% 
+  setView(lng = mean(data$Longitude),
+          lat = mean(data$Latitude),
+          zoom = 12)
+        
+#Adding various ESRI background layers
+esri <- grep("^Esri", providers, value = TRUE)
+# Removing layers which have no available data for our specific area 
+esri <- esri[esri != "Esri.WorldTerrain"]
+esri <-  esri[esri != "Esri.OceanBasemap"]
+esri <-  esri[esri != "Esri.DeLorme"]
+esri <-  esri[esri != "Esri.WorldPhysical"]
+
+for (provider in esri) {
+  center_location <- center_location %>% addProviderTiles(provider, group = provider)
+}
+
+
+# Making basic map
+BlueMounatains_map <- center_location %>%
+  addLayersControl(baseGroups = names(esri),
+                   options = layersControlOptions(collapsed = FALSE)) %>%
+  # Adding minimap
+  addMiniMap(tiles = esri[[1]], toggleDisplay = TRUE,
+             position = "bottomright") %>%
+  # Enabling measurements
+  addMeasure(
+    position = "bottomleft",
+    primaryLengthUnit = "meters",
+    primaryAreaUnit = "sqmeters",
+    activeColor = "#3D535D",
+    completedColor = "#7D4479") %>% 
+  htmlwidgets::onRender("
+                        function(el, x) {
+                        var myMap = this;
+                        myMap.on('baselayerchange',
+                        function (e) {
+                        myMap.minimap.changeLayer(L.tileLayer.provider(e.name));
+                        })
+                        }") %>% 
+  addControl("", position = "topright")
+
+BlueMounatains_map
+
+
+# Adding interactive makers
+BlueMounatains_map_with_markers <- BlueMounatains_map %>% 
+  addTiles() %>% 
+  # Adding markers
+  addMarkers(lng = data$Longitude, 
+             lat = data$Latitude,
+             popup = paste("ID:", data$FeatureID, "<br/>",
+                           "Type:", data$FeatureType, "<br/>",
+                           "Description:", data$Description,"<br/>",
+                           "Coordinates:", data$Latitude,",", data$Longitude)
+             )
+
+# Showing plot
+BlueMounatains_map_with_markers
+
+
+# Clustering markers
+BlueMounatains_map_with_clustering <- BlueMounatains_map %>% 
+  addTiles() %>% 
+  addMarkers(lng = data$Longitude, 
+             lat = data$Latitude,s
+             popup = paste("ID:", data$FeatureID, "<br/>",
+                           "Type:", data$FeatureType, "<br/>",
+                           "Description:", data$Description,"<br/>",
+                           "Coordinates:", data$Latitude,",", data$Longitude),
+             clusterOptions = markerClusterOptions()
+             )
+
+# Showing plot
+BlueMounatains_map_with_clustering
+
+# Saving the map
+saveWidget(BlueMounatains_map_with_markers, "Interactive-Map.html", selfcontained = TRUE)
+
+#### CONCLUSION ####
+# Clustering the individual locations together provides makes the map more visually pleasing as many points are chaotically located close together. 
+# However, applying this funciton makes it impossible to see the exact location of each point and, therefore, I would not recommend it.
